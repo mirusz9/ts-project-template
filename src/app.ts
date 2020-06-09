fa1b0296739859eba4221cc237c21be1d75b5652
mirusz9 <adam.sztano@gmail.com>
2020-06-09 13:47:48 +0200

    created lots of files

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+
+app.get("/", (req, res) => {
+	res.sendFile("index.html");
+});
+
+app.use((req, res, next) => {
+	res.redirect("/");
+	res.end();
+	next();
+})
+
+app.listen(port, (err) => {
+	if (err) throw err;
+	console.log(`Listening on localhost:${port}`);
+});
